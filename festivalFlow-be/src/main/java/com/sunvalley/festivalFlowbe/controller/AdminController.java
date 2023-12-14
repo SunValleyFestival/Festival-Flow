0106e5b1325d4aaa11c84840c0afcacaf7484173
grata <aramis.grata@bluewin.ch>
2023-12-14 16:52:01 +0100

    added CORS management

--- festivalFlow-be/src/main/java/com/sunvalley/festivalFlowbe/controller/AdminController.java
+++ festivalFlow-be/src/main/java/com/sunvalley/festivalFlowbe/controller/AdminController.java
@@ -2,6 +2,7 @@ package com.sunvalley.festivalFlowbe.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/admin")
 public class AdminController {
 
+  @CrossOrigin
   @GetMapping("")
   public ResponseEntity<HttpStatus> get(){
     return ResponseEntity.ok(HttpStatus.OK);
