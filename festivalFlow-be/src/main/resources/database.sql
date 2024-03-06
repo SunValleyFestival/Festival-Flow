5a6ee228e42d7757f2577d29e81fc1295dff9854
grata <aramis.grata@bluewin.ch>
2024-03-06 19:59:13 +0100

    feat(DEV): fixed sql naming convention

--- festivalFlow-be/src/main/resources/database.sql
+++ festivalFlow-be/src/main/resources/database.sql
@@ -27,9 +27,9 @@ create table shift
     description     varchar(1000),
     name            varchar(20)        not null,
     location_id     int,
-    startTime       time               not null,
-    endTime         time               not null,
-    maxCollaborator int                not null,
+    start_time       time               not null,
+    end_time         time               not null,
+    max_collaborator int                not null,
     primary key (id),
     foreign key (location_id) references location (id)
 );
@@ -38,8 +38,8 @@ create table collaborator
 (
     id        int auto_increment not null,
     phone     varchar(11)        not null,
-    firstName varchar(20),
-    lastName  varchar(20),
+    first_name varchar(20),
+    last_name  varchar(20),
     age       date,
     size      varchar(3),
     email     varchar(100),
@@ -58,7 +58,7 @@ create table association
 CREATE VIEW shift_availability AS
 SELECT
     s.id AS shift_id,
-    s.maxCollaborator - COUNT(a.collaborator_id) AS available_slots,
+    s.max_collaborator - COUNT(a.collaborator_id) AS available_slots,
     s.location_id as location_id
 FROM shift s
          LEFT JOIN association a ON s.id = a.shift_id
@@ -80,30 +80,30 @@ values ('Third Stage', 'Third stage of the festival', 2);
 insert into location (name, description, day_id)
 values ('Fourth Stage', 'Fourth stage of the festival', 3);
 
-insert into shift (description, name, location_id, startTime, endTime, maxCollaborator)
+insert into shift (description, name, location_id, start_time, end_time, max_collaborator)
 values ('First shift of the day', 'First shift', 1, '12:00:00', '14:00:00', 5);
-insert into shift (description, name, location_id, startTime, endTime, maxCollaborator)
+insert into shift (description, name, location_id, start_time, end_time, max_collaborator)
 values ('Second shift of the day', 'Second shift', 1, '14:00:00', '16:00:00', 5);
-insert into shift (description, name, location_id, startTime, endTime, maxCollaborator)
+insert into shift (description, name, location_id, start_time, end_time, max_collaborator)
 values ('Third shift of the day', 'Third shift', 1, '16:00:00', '18:00:00', 5);
-insert into shift (description, name, location_id, startTime, endTime, maxCollaborator)
+insert into shift (description, name, location_id, start_time, end_time, max_collaborator)
 values ('First shift of the day', 'First shift', 2, '12:00:00', '14:00:00', 5);
-insert into shift (description, name, location_id, startTime, endTime, maxCollaborator)
+insert into shift (description, name, location_id, start_time, end_time, max_collaborator)
 values ('Second shift of the day', 'Second shift', 2, '14:00:00', '16:00:00', 5);
-insert into shift (description, name, location_id, startTime, endTime, maxCollaborator)
+insert into shift (description, name, location_id, start_time, end_time, max_collaborator)
 values ('Third shift of the day', 'Third shift', 3, '16:00:00', '18:00:00', 5);
-insert into shift (description, name, location_id, startTime, endTime, maxCollaborator)
+insert into shift (description, name, location_id, start_time, end_time, max_collaborator)
 values ('First shift of the day', 'First shift', 4, '12:00:00', '14:00:00', 5);
 
-insert into collaborator (phone, firstName, lastName, age, size, email)
+insert into collaborator (phone, first_name, last_name, age, size, email)
 values ('0606060606', 'Il', 'Pirla', '1990-01-01', 'M', 'joekueng05@gmail.com');
-insert into collaborator (phone, firstName, lastName, age, size, email)
+insert into collaborator (phone, first_name, last_name, age, size, email)
 values ('0606060607', 'Jane', 'Doe', '1990-01-01', 'S', 'jane.doe@gmail.com');
-insert into collaborator (phone, firstName, lastName, age, size, email)
+insert into collaborator (phone, first_name, last_name, age, size, email)
 values ('0606060608', 'Bob', 'Smith', '1990-01-01', 'M', 'bob.smith@gmail.com');
-insert into collaborator (phone, firstName, lastName, age, size, email)
+insert into collaborator (phone, first_name, last_name, age, size, email)
 values ('0606060609', 'Alice', 'Johnson', '1990-01-01', 'L', 'alice.johnson@gmail.com');
-insert into collaborator (phone, firstName, lastName, age, size, email)
+insert into collaborator (phone, first_name, last_name, age, size, email)
 values ('0606060610', 'Charlie', 'Brown', '1990-01-01', 'XL', 'charlie.brown@gmail.com');
 
 insert into association (shift_id, collaborator_id, status)
