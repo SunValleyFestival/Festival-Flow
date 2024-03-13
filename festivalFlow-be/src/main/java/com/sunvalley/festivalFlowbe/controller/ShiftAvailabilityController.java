d8f4b4e569af1a236e948bffa30ec8c630cdf98f
joeküng <joekueng05@gmail>
2024-03-13 22:16:09 +0100

    feat(58): ShiftAvailabilityController

--- festivalFlow-be/src/main/java/com/sunvalley/festivalFlowbe/controller/ShiftAvailabilityController.java
+++ festivalFlow-be/src/main/java/com/sunvalley/festivalFlowbe/controller/ShiftAvailabilityController.java
@@ -1,6 +1,7 @@
 package com.sunvalley.festivalFlowbe.controller;
 
-import com.sunvalley.festivalFlowbe.service.AssociationService;
+import com.sunvalley.festivalFlowbe.entity.ShiftAvailabilityView;
+import com.sunvalley.festivalFlowbe.service.ShiftAvailabilityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,20 +11,31 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/festival-flow/")
 public class ShiftAvailabilityController {
 
-    private static final String SHIFT_AVAILABILITY = "user/shift-availability/";
+  private static final String ADMIN = "/admin/shift-availability/";
+  private static final String SHIFT_AVAILABILITY = "user/shift-availability/";
 
-    private final AssociationService associationService;
+  private final ShiftAvailabilityService shiftAvailabilityService;
 
-    @Autowired
-    public ShiftAvailabilityController(AssociationService associationService) {
-        this.associationService = associationService;
-    }
+  @Autowired
+  public ShiftAvailabilityController(ShiftAvailabilityService shiftAvailabilityService) {
+    this.shiftAvailabilityService = shiftAvailabilityService;
+  }
 
-    @CrossOrigin
-    @GetMapping(SHIFT_AVAILABILITY + "{shiftId}")
-    public ResponseEntity<Integer> getShift(@PathVariable int shiftId) {
-        Integer shiftAvailability = associationService.countByShiftIdAndStatusNotReject(shiftId);
-        return new ResponseEntity<>(shiftAvailability, HttpStatus.OK);
-    }
+  @CrossOrigin
+  @GetMapping(SHIFT_AVAILABILITY + "{shiftId}")
+  public ResponseEntity<ShiftAvailabilityView> getShiftAvailability(@PathVariable int shiftId) {
+    var shiftAvailability = shiftAvailabilityService.getByShiftId(shiftId);
+    return new ResponseEntity<>(shiftAvailability, HttpStatus.OK);
+  }
+
+  @CrossOrigin
+  @GetMapping(SHIFT_AVAILABILITY + "location/{locationId}")
+  public ResponseEntity<ShiftAvailabilityView> getAvailableSlotsByLocationId(@PathVariable int locationId) {
+    var availableSlots = shiftAvailabilityService.getAvailableSlotsByLocationId(locationId);
+    var shiftAvailability = new ShiftAvailabilityView();
+    shiftAvailability.setAvailableSlots(availableSlots);
+    shiftAvailability.setLocationId(locationId);
+    return new ResponseEntity<>(shiftAvailability, HttpStatus.OK);
+  }
 
 }
---
+++ festivalFlow-be/src/main/java/com/sunvalley/festivalFlowbe/entity/ShiftAvailabilityView.java
@@ -0,0 +1,21 @@
+package com.sunvalley.festivalFlowbe.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity(name = "shift_availability")
+@Data
+public class ShiftAvailabilityView {
+
+    @Id
+    @Column(name = "shift_id")
+    private int shiftId;
+
+    @Column(name = "available_slots")
+    private int availableSlots;
+
+    @Column(name = "location_id")
+    private int locationId;
+}
