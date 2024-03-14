4fca6834bad815c05ceab3558629c3162d0a65fb
grata <aramis.grata@bluewin.ch>
2024-03-14 16:27:13 +0100

    fix(56): Status

--- festivalFlow-be/src/main/java/com/sunvalley/festivalFlowbe/entity/Status.java
+++ festivalFlow-be/src/main/java/com/sunvalley/festivalFlowbe/entity/Status.java
@@ -4,13 +4,7 @@ import lombok.Getter;
 
 @Getter
 public enum Status {
-    PENDING(0),
-    ACCEPTED(1),
-    REJECTED(2);
-
-    private final int value;
-
-    Status(int value) {
-        this.value = value;
-    }
+    PENDING,
+    ACCEPTED,
+    REJECTED
 }
