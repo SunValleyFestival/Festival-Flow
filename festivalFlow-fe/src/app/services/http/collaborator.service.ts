c9e240dc89fd5c089a1f4a4475a706ab0604960e
joeküng <joekueng05@gmail>
2024-03-14 17:48:59 +0100

    fix(58): fix only admin operation

--- festivalFlow-fe/src/app/services/http/collaborator.service.ts
+++ festivalFlow-fe/src/app/services/http/collaborator.service.ts
@@ -2,8 +2,10 @@ import {Injectable} from '@angular/core';
 import {environment} from '../../../environments/environment';
 import {Collaborator} from "../../interfaces/CollaboratorEntity";
 import {HttpAuthClient} from "./token/http-auth-client";
+import {Observable} from "rxjs";
 
 const BASE_URL = environment.userBaseUrl + "/collaborator/";
+const ADMIN_BASE_URL = environment.adminBaseUrl + "/collaborator/";
 @Injectable({
   providedIn: 'root'
 })
@@ -12,15 +14,15 @@ export class CollaboratorService {
   constructor(private http: HttpAuthClient) {
   }
 
-  getCollaborators() {
-    return this.http.get(BASE_URL);
+  getCollaborators(): Observable<Collaborator[]>{
+    return this.http.get(ADMIN_BASE_URL);
   }
 
   updateCollaborator(collaborator: Collaborator) {
     return this.http.put(BASE_URL + "update", collaborator);
   }
 
-  getCollaboratorById(id: number) {
+  getCollaboratorById(id: number): Observable<Collaborator> {
     return this.http.get(BASE_URL + id);
   }
 
