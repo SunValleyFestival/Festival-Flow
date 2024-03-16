148a02dfff85e25d64738fac9f127db71150662e
joeküng <joekueng05@gmail>
2024-03-16 15:44:44 +0100

    feat(45): first approach export service

--- festivalFlow-be/src/main/java/com/sunvalley/festivalFlowbe/controller/utility/AdminController.java
+++ festivalFlow-be/src/main/java/com/sunvalley/festivalFlowbe/controller/utility/AdminController.java
@@ -1,21 +1,25 @@
 package com.sunvalley.festivalFlowbe.controller.utility;
 
 import com.sunvalley.festivalFlowbe.entity.utility.AuthEntity;
+import com.sunvalley.festivalFlowbe.service.utility.ExportService;
 import com.sunvalley.festivalFlowbe.service.utility.JWTTokenProviderService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+
 @RestController
-@RequestMapping("/festival-flow/")
+@RequestMapping("/festival-flow/admin/")
 @RequiredArgsConstructor
 @Slf4j
 public class AdminController {
 
     private final JWTTokenProviderService tokenProvider;
+    private final ExportService exportService;
 
     @CrossOrigin
-    @GetMapping("admin/token")
+    @GetMapping("token")
     public AuthEntity login() {
         AuthEntity authEntity = new AuthEntity();
         authEntity.setToken(tokenProvider.generateTokenForAdmin());
@@ -23,9 +27,10 @@ public class AdminController {
     }
 
     @CrossOrigin
-    @PostMapping("admin/export")
-    public String loginConfirm() {
-        return "not implmented yet";
+    @GetMapping("export")
+    public String loginConfirm() throws IOException {
+        exportService.export();
+        return null;
     }
 
 
---
+++ festivalFlow-be/src/main/java/com/sunvalley/festivalFlowbe/service/utility/ExportService.java
@@ -0,0 +1,78 @@
+package com.sunvalley.festivalFlowbe.service.utility;
+
+import com.sunvalley.festivalFlowbe.entity.CollaboratorEntity;
+import com.sunvalley.festivalFlowbe.repository.CollaboratorRepository;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFFont;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.List;
+
+@Service
+public class ExportService {
+
+    private final CollaboratorRepository collaboratorRepository;
+
+    @Autowired
+    public ExportService(CollaboratorRepository collaboratorRepository) {
+        this.collaboratorRepository = collaboratorRepository;
+    }
+
+
+    public void export() throws IOException {
+        Workbook workbook = new XSSFWorkbook();
+
+        Sheet sheet = workbook.createSheet("Persons");
+        sheet.setColumnWidth(0, 6000);
+        sheet.setColumnWidth(1, 4000);
+
+        Row header = sheet.createRow(0);
+
+        CellStyle headerStyle = workbook.createCellStyle();
+        headerStyle.setFillForegroundColor(IndexedColors.LIGHT_BLUE.getIndex());
+        headerStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+
+        XSSFFont font = ((XSSFWorkbook) workbook).createFont();
+        font.setFontName("Arial");
+        font.setFontHeightInPoints((short) 16);
+        font.setBold(true);
+        headerStyle.setFont(font);
+
+        Cell headerCell = header.createCell(0);
+        headerCell.setCellValue("Name");
+        headerCell.setCellStyle(headerStyle);
+
+        headerCell = header.createCell(1);
+        headerCell.setCellValue("Age");
+        headerCell.setCellStyle(headerStyle);
+
+        CellStyle style = workbook.createCellStyle();
+        style.setWrapText(true);
+
+        Row row = sheet.createRow(2);
+        Cell cell = row.createCell(0);
+        cell.setCellValue("John Smith");
+        cell.setCellStyle(style);
+
+        cell = row.createCell(1);
+        cell.setCellValue(20);
+        cell.setCellStyle(style);
+
+        File currDir = new File(".");
+        String path = currDir.getAbsolutePath();
+        String fileLocation = path.substring(0, path.length() - 1) + "temp.xlsx";
+
+        FileOutputStream outputStream = new FileOutputStream(fileLocation);
+        workbook.write(outputStream);
+        workbook.close();
+    }
+
+
+
+
+}
