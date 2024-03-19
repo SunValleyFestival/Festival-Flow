c53c51dc373a0af93401819c80a73ad9a5ceff7c
joeküng <joekueng05@gmail>
2024-03-19 18:41:44 +0100

    feat(45): sheet collaboratori X

--- festivalFlow-be/src/main/java/com/sunvalley/festivalFlowbe/service/AssociationService.java
+++ festivalFlow-be/src/main/java/com/sunvalley/festivalFlowbe/service/AssociationService.java
@@ -4,65 +4,72 @@ import com.sunvalley.festivalFlowbe.entity.AssociationAdmin;
 import com.sunvalley.festivalFlowbe.entity.AssociationEntity;
 import com.sunvalley.festivalFlowbe.entity.CollaboratorEntity;
 import com.sunvalley.festivalFlowbe.repository.AssociationRepository;
-import java.util.ArrayList;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class AssociationService {
 
-  private final AssociationRepository associationRepository;
-  private final CollaboratorService collaboratorService;
+    private final AssociationRepository associationRepository;
+    private final CollaboratorService collaboratorService;
 
-  @Autowired
-  public AssociationService(AssociationRepository associationRepository, CollaboratorService collaboratorService) {
-    this.associationRepository = associationRepository;
-    this.collaboratorService = collaboratorService;
-  }
+    @Autowired
+    public AssociationService(AssociationRepository associationRepository, CollaboratorService collaboratorService) {
+        this.associationRepository = associationRepository;
+        this.collaboratorService = collaboratorService;
+    }
 
-  public List<AssociationEntity> getByCollaboratorId(int id) {
-    return associationRepository.findByCollaboratorId(id);
-  }
+    public List<AssociationEntity> getByCollaboratorId(int id) {
+        return associationRepository.findByCollaboratorId(id);
+    }
 
-  public AssociationEntity getByCollaboratorIdAndShiftId(int collaboratorId, int shiftId) {
-    return associationRepository.findByIdCollaboratorIdAndIdShiftId(collaboratorId, shiftId);
-  }
+    public AssociationEntity getByCollaboratorIdAndShiftId(int collaboratorId, int shiftId) {
+        return associationRepository.findByIdCollaboratorIdAndIdShiftId(collaboratorId, shiftId);
+    }
 
-  public List<AssociationEntity> getAll() {
-    return associationRepository.findAll();
-  }
+    public List<AssociationEntity> getAll() {
+        return associationRepository.findAll();
+    }
 
-  public void save(AssociationEntity association) {
-    associationRepository.save(association);
-  }
+    public void save(AssociationEntity association) {
+        associationRepository.save(association);
+    }
 
-  public Integer countByShiftIdAndStatusNotReject(int shiftId) {
-    return associationRepository.countByShiftIdAndStatusNotReject(shiftId);
-  }
+    public Integer countByShiftIdAndStatusNotReject(int shiftId) {
+        return associationRepository.countByShiftIdAndStatusNotReject(shiftId);
+    }
 
     public List<Integer> getByUserId(int userId) {
         return associationRepository.findByUserId(userId);
     }
 
-  public List<AssociationEntity> getByShiftId(final int locationId) {
-    return associationRepository.findByShiftId(locationId);
-  }
-
-  public List<CollaboratorEntity> getCollaboratorsByShiftId(final int shiftId) {
-    return associationRepository.getCollaboratorsByShiftId(shiftId);
-  }
-
-  public List<AssociationAdmin> getAssociationAdminByShiftId(final int shiftId) {
-    var associationsAdmin = new ArrayList<AssociationAdmin>();
-    var associations = this.getByShiftId(shiftId);
-
-    for (AssociationEntity association : associations) {
-      var collaborator = collaboratorService.getById(association.getId().getCollaboratorId());
-      var associationAdmin = new AssociationAdmin(collaborator, association.getId().getShiftId(), association.getStatus());
-      associationsAdmin.add(associationAdmin);
+    public List<AssociationEntity> getByShiftId(final int locationId) {
+        return associationRepository.findByShiftId(locationId);
     }
 
-    return associationsAdmin;
-  }
+    public List<CollaboratorEntity> getCollaboratorsByShiftId(final int shiftId) {
+        return associationRepository.getCollaboratorsByShiftId(shiftId);
+    }
+
+    public List<AssociationAdmin> getAssociationAdminByShiftId(final int shiftId) {
+        var associationsAdmin = new ArrayList<AssociationAdmin>();
+        var associations = this.getByShiftId(shiftId);
+
+        for (AssociationEntity association : associations) {
+            var collaborator = collaboratorService.getById(association.getId().getCollaboratorId());
+            var associationAdmin = new AssociationAdmin(collaborator, association.getId().getShiftId(), association.getStatus());
+            associationsAdmin.add(associationAdmin);
+        }
+
+        return associationsAdmin;
+    }
+
+    public boolean existsByCollaboratorIdAndShiftIdAndAccepted(int collaboratorId, int shiftId) {
+        if (associationRepository.existsByCollaboratorIdAndShiftIdAndAccepted(collaboratorId, shiftId).isEmpty()) {
+            return false;
+        } else return true;
+    }
 }
--- festivalFlow-be/src/main/java/com/sunvalley/festivalFlowbe/service/utility/ExportService.java
+++ festivalFlow-be/src/main/java/com/sunvalley/festivalFlowbe/service/utility/ExportService.java
@@ -4,20 +4,11 @@ import com.sunvalley.festivalFlowbe.entity.CollaboratorEntity;
 import com.sunvalley.festivalFlowbe.entity.DayEntity;
 import com.sunvalley.festivalFlowbe.entity.LocationEntity;
 import com.sunvalley.festivalFlowbe.entity.ShiftEntity;
-import com.sunvalley.festivalFlowbe.repository.CollaboratorRepository;
-import com.sunvalley.festivalFlowbe.repository.DayRepository;
-import com.sunvalley.festivalFlowbe.repository.LocationRepository;
-import com.sunvalley.festivalFlowbe.repository.ShiftRepository;
-import com.sunvalley.festivalFlowbe.service.CollaboratorService;
-import com.sunvalley.festivalFlowbe.service.DayService;
-import com.sunvalley.festivalFlowbe.service.LocationService;
-import com.sunvalley.festivalFlowbe.service.ShiftService;
-import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import com.sunvalley.festivalFlowbe.service.*;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.ss.util.CellRangeAddress;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.stereotype.Service;
 
 import java.io.File;
@@ -30,29 +21,31 @@ import java.util.*;
 public class ExportService {
 
 
-   private final CollaboratorService collaboratorService;
-   private final DayService dayService;
+    private final CollaboratorService collaboratorService;
+    private final DayService dayService;
     private final LocationService locationService;
     private final ShiftService shiftService;
+    private final AssociationService associationService;
 
 
     @Autowired
-    public ExportService(CollaboratorService collaboratorService, DayService dayService, LocationService locationService, ShiftService shiftService){
-
+    public ExportService(CollaboratorService collaboratorService, DayService dayService, LocationService locationService, ShiftService shiftService, AssociationService associationService) {
         this.collaboratorService = collaboratorService;
         this.dayService = dayService;
         this.locationService = locationService;
         this.shiftService = shiftService;
+        this.associationService = associationService;
     }
 
 
-    public void export() throws IOException, InvalidFormatException {
+    public void export() throws IOException {
         Workbook workbook = new XSSFWorkbook();
-        Sheet sheet = workbook.createSheet("collaborator");
+        Sheet sheetCollaboratori = workbook.createSheet("Collaboratori");
+
         List<CollaboratorEntity> collaboratorEntities = collaboratorService.findCollaboratorEntitiesWhereIsPopulatedAndAssociationAccepted();
 
         for (int i = 0; i < collaboratorEntities.size(); i++) {
-            Row row = sheet.createRow(i + 1);
+            Row row = sheetCollaboratori.createRow(i + 3);
             Cell indice = row.createCell(0);
             indice.setCellValue(i + 1);
             Cell id = row.createCell(1);
@@ -65,7 +58,6 @@ public class ExportService {
             town.setCellValue(collaboratorEntities.get(i).getTown());
             Cell age = row.createCell(5);
             int ageNum = getAge(collaboratorEntities.get(i).getAge());
-
             age.setCellValue(ageNum);
             Cell phone = row.createCell(6);
             phone.setCellValue(collaboratorEntities.get(i).getPhone());
@@ -87,18 +79,14 @@ public class ExportService {
                 ageUnder.setCellValue("X");
             }
 
-
             String firstNameLastName = collaboratorEntities.get(i).getFirstName() + " " + collaboratorEntities.get(i).getLastName();
             Cell nameSurname = row.createCell(11);
             nameSurname.setCellValue(firstNameLastName);
 
         }
 
-        Row row = sheet.createRow(0);
-
         List<DayEntity> dayEntities = dayService.getAll();
-        int dayCount= dayEntities.size();
-
+        int dayCount = dayEntities.size();
 
         Map<Integer, List<ShiftEntity>> shiftsByDay = new HashMap<>();
 
@@ -112,75 +100,91 @@ public class ExportService {
                 }
             }
         }
-        int index = 0;
-        for (DayEntity dayEntity : dayEntities) {
-            for (int j = 0; j < shiftsByDay.get(dayEntity.getId()).size(); j++) {
-                index++;
-                row.createCell(14 + index).setCellValue(shiftsByDay.get(dayEntity.getId()).get(j).getName() + "day: " + dayEntity.getId() + "location: " + shiftsByDay.get(dayEntity.getId()).get(j).getLocation().getName());
-
-            }
-        }
-
-
-        Sheet bylocationSheet = workbook.createSheet("CollaboratoriPerTurno");
-
         CellStyle shiftNameStyle = workbook.createCellStyle();
         Font font = workbook.createFont();
         font.setBold(true);
         shiftNameStyle.setFont(font);
 
-        // Creazione dell'indice
+
+        Row rowDayCollaboratori = sheetCollaboratori.createRow(0);
+        Row rowLocationCollaboratori = sheetCollaboratori.createRow(1);
+        Row rowShiftCollaboratori = sheetCollaboratori.createRow(2);
+        int h = 15;
+        for (DayEntity dayEntity : dayService.getAll()) {
+            Cell cellDay = rowDayCollaboratori.createCell(h);
+            cellDay.setCellValue(dayEntity.getName());
+            cellDay.setCellStyle(shiftNameStyle);
+            int firstCol = h;
+            for (LocationEntity locationEntity : locationService.getLocationsByDayId(dayEntity.getId())) {
+                Cell cellLocation = rowLocationCollaboratori.createCell(h);
+                cellLocation.setCellValue(locationEntity.getName());
+                cellLocation.setCellStyle(shiftNameStyle);
+                int firstColLocation = h;
+                for (ShiftEntity shiftEntity : shiftService.getShiftsByLocationId(locationEntity.getId())) {
+                    Cell cell = rowShiftCollaboratori.createCell(h);
+                    cell.setCellValue(shiftEntity.getName());
+                    cell.setCellStyle(shiftNameStyle);
+                    for (int o = 0; o < collaboratorEntities.size(); o++) {
+                        if (associationService.existsByCollaboratorIdAndShiftIdAndAccepted(collaboratorEntities.get(o).getId(), shiftEntity.getId())) {
+                            Row rowCollaborator = sheetCollaboratori.getRow(o + 3);
+                            Cell cellCollaborator = rowCollaborator.createCell(h);
+                            cellCollaborator.setCellValue("X");
+                        }
+                    }
+                    h++;
+                }
+                sheetCollaboratori.addMergedRegion(new CellRangeAddress(1, 1, firstColLocation, h - 1));
+            }
+            sheetCollaboratori.addMergedRegion(new CellRangeAddress(0, 0, firstCol, h - 1));
+        }
+
+
+        Sheet bylocationSheet = workbook.createSheet("CollaboratoriPerTurno");
+
+
         for (int i = 0; i < 100; i++) {
             Row row1 = bylocationSheet.createRow(i);
-            Cell cell = row1.createCell(0); // L'indice viene posizionato nella colonna 0
+            Cell cell = row1.createCell(0);
             cell.setCellValue(i);
         }
 
-        Row rowDay= bylocationSheet.getRow(0);
-        Row rowLocation= bylocationSheet.getRow(1);
+        Row rowDay = bylocationSheet.getRow(0);
+        Row rowLocation = bylocationSheet.getRow(1);
         Row rowShift = bylocationSheet.getRow(2);
-
-
-        int h=1;
-
-        //sheet.addMergedRegion(new CellRangeAddress(firstRow, lastRow, firstCol, lastCol));
-
-        for (DayEntity dayEntity: dayService.getAll()) {
+        h = 1;
+        for (DayEntity dayEntity : dayService.getAll()) {
             Cell cellDay = rowDay.createCell(h);
             cellDay.setCellValue(dayEntity.getName());
             cellDay.setCellStyle(shiftNameStyle);
             int firstCol = h;
-            for (LocationEntity locationEntity: locationService.getLocationsByDayId(dayEntity.getId())){
+            for (LocationEntity locationEntity : locationService.getLocationsByDayId(dayEntity.getId())) {
                 Cell cellLocation = rowLocation.createCell(h);
                 cellLocation.setCellValue(locationEntity.getName());
                 cellLocation.setCellStyle(shiftNameStyle);
-                int firstColLocation= h;
-                for (ShiftEntity shiftEntity: shiftService.getShiftsByLocationId(locationEntity.getId())){
+                int firstColLocation = h;
+                for (ShiftEntity shiftEntity : shiftService.getShiftsByLocationId(locationEntity.getId())) {
                     Cell cell = rowShift.createCell(h);
                     cell.setCellValue(shiftEntity.getName());
                     cell.setCellStyle(shiftNameStyle);
-                    int k=3;
-                    for (CollaboratorEntity collaboratorEntity: collaboratorService.findCollaboratorEntitiesWhereIsPopulatedAndAssociationAcceptedByShiftId(shiftEntity.getId())) {
-                        Row rowCollaborator= bylocationSheet.getRow(k);
-                        Cell cellCollaborator= rowCollaborator.createCell(h);
+                    int k = 3;
+                    for (CollaboratorEntity collaboratorEntity : collaboratorService.findCollaboratorEntitiesWhereIsPopulatedAndAssociationAcceptedByShiftId(shiftEntity.getId())) {
+                        Row rowCollaborator = bylocationSheet.getRow(k);
+                        Cell cellCollaborator = rowCollaborator.createCell(h);
                         cellCollaborator.setCellValue(collaboratorEntity.getFirstName() + " " + collaboratorEntity.getLastName());
                         k++;
                     }
                     h++;
                 }
-                bylocationSheet.addMergedRegion(new CellRangeAddress(1, 1, firstColLocation, h-1));
-
+                bylocationSheet.addMergedRegion(new CellRangeAddress(1, 1, firstColLocation, h - 1));
             }
-            bylocationSheet.addMergedRegion(new CellRangeAddress(0, 0, firstCol, h-1));
+            bylocationSheet.addMergedRegion(new CellRangeAddress(0, 0, firstCol, h - 1));
         }
 
 
+        File exportDir = new File("export");
 
-        File exportDir = new File("export"); // Create a File object for the "export" directory
-
-// Check if the directory exists (optional, but recommended for robustness)
         if (!exportDir.exists()) {
-            exportDir.mkdirs(); // Create the directory if it doesn't exist
+            exportDir.mkdirs();
         }
         Calendar calendar = Calendar.getInstance();
         calendar.get(Calendar.HOUR_OF_DAY);
@@ -190,9 +194,7 @@ public class ExportService {
         FileOutputStream outputStream = new FileOutputStream(day);
         workbook.write(outputStream);
         workbook.close();
-
     }
-
 
     private File readFile() {
         return new File("Pianif-turni-svf_vuoto.xlsx");
