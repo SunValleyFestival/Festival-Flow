ceba6c081cc6faa2fb509b3e867dc6818f192698
grata <aramis.grata@bluewin.ch>
2024-03-21 13:28:02 +0100

    feat(78): FE checks on create user details

--- festivalFlow-be/src/main/java/com/sunvalley/festivalFlowbe/controller/CollaboratorController.java
+++ festivalFlow-be/src/main/java/com/sunvalley/festivalFlowbe/controller/CollaboratorController.java
@@ -19,80 +19,83 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @Slf4j
 @RestController
 @RequestMapping("/festival-flow/")
 public class CollaboratorController {
 
-    private static final String ADMIN = "admin/collaborator/";
-    private static final String COLLABORATOR = "user/collaborator/";
+  private static final String ADMIN = "admin/collaborator/";
+  private static final String COLLABORATOR = "user/collaborator/";
 
-    private final CollaboratorService collaboratorService;
-    private final JWTTokenProviderService jwtTokenProviderService;
+  private final CollaboratorService collaboratorService;
+  private final JWTTokenProviderService jwtTokenProviderService;
 
-    @Autowired
-    public CollaboratorController(CollaboratorService collaboratorService, JWTTokenProviderService jwtTokenProviderService) {
-        this.collaboratorService = collaboratorService;
-        this.jwtTokenProviderService = jwtTokenProviderService;
+  @Autowired
+  public CollaboratorController(CollaboratorService collaboratorService, JWTTokenProviderService jwtTokenProviderService) {
+    this.collaboratorService = collaboratorService;
+    this.jwtTokenProviderService = jwtTokenProviderService;
+  }
+
+  @CrossOrigin
+  @GetMapping(COLLABORATOR)
+  public ResponseEntity<CollaboratorEntity> get(@RequestHeader("Authorization") String token) throws ParseException {
+    CollaboratorEntity collaborator = collaboratorService.getById(jwtTokenProviderService.getUserIdFromToken(token));
+    return new ResponseEntity<>(collaborator, HttpStatus.OK);
+  }
+
+  @CrossOrigin
+  @GetMapping(COLLABORATOR + "{id}")
+  public ResponseEntity<CollaboratorEntity> getById(@PathVariable int id) {
+    CollaboratorEntity collaborator = collaboratorService.getById(id);
+    return new ResponseEntity<>(collaborator, HttpStatus.OK);
+  }
+
+  @CrossOrigin
+  @GetMapping(COLLABORATOR + "all")
+  public ResponseEntity<List<CollaboratorEntity>> getAll() {
+    List<CollaboratorEntity> collaborators = collaboratorService.getAll();
+    return new ResponseEntity<>(collaborators, HttpStatus.OK);
+  }
+
+  //cambiare e prendere email
+
+  @CrossOrigin
+  @PutMapping(COLLABORATOR + "update")
+  public ResponseEntity<CollaboratorEntity> update(@RequestBody CollaboratorEntity collaborator, @RequestHeader("Authorization") String token) throws ParseException {
+    if (!jwtTokenProviderService.getUserIdFromToken(token).equals(collaborator.getId())) {
+      return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+    } else {
+      CollaboratorEntity newCollaborator = collaboratorService.update(collaborator);
+      return new ResponseEntity<>(newCollaborator, HttpStatus.OK);
     }
+  }
 
-    @CrossOrigin
-    @GetMapping(COLLABORATOR)
-    public ResponseEntity<CollaboratorEntity> get(@RequestHeader("Authorization") String token) throws ParseException {
-        CollaboratorEntity collaborator = collaboratorService.getById(jwtTokenProviderService.getUserIdFromToken(token));
-        return new ResponseEntity<>(collaborator, HttpStatus.OK);
-    }
+  @CrossOrigin
+  @PutMapping(ADMIN + "update")
+  public ResponseEntity<CollaboratorEntity> updateAdmin(@RequestBody CollaboratorEntity collaborator) {
+    CollaboratorEntity newCollaborator = collaboratorService.update(collaborator);
+    return new ResponseEntity<>(newCollaborator, HttpStatus.OK);
+  }
 
-    @CrossOrigin
-    @GetMapping(COLLABORATOR + "{id}")
-    public ResponseEntity<CollaboratorEntity> getById(@PathVariable int id) {
-        CollaboratorEntity collaborator = collaboratorService.getById(id);
-        return new ResponseEntity<>(collaborator, HttpStatus.OK);
-    }
+  @CrossOrigin
+  @GetMapping(ADMIN)
+  public ResponseEntity<List<CollaboratorEntity>> getAllAdmin() {
+    List<CollaboratorEntity> collaborators = collaboratorService.findCollaboratorEntitiesWhereIsPopulated();
+    return new ResponseEntity<>(collaborators, HttpStatus.OK);
+  }
 
-    @CrossOrigin
-    @GetMapping(COLLABORATOR + "all")
-    public ResponseEntity<List<CollaboratorEntity>> getAll() {
-        List<CollaboratorEntity> collaborators = collaboratorService.getAll();
-        return new ResponseEntity<>(collaborators, HttpStatus.OK);
-    }
+  @CrossOrigin
+  @GetMapping(ADMIN + "shift/{shiftId}")
+  public ResponseEntity<List<CollaboratorEntity>> getByShiftId(@PathVariable int shiftId) {
+    List<CollaboratorEntity> collaborators = collaboratorService.getByShiftId(shiftId);
+    return new ResponseEntity<>(collaborators, HttpStatus.OK);
+  }
 
-
-    //cambiare e prendere email
-
-    @CrossOrigin
-    @PutMapping(COLLABORATOR + "update")
-    public ResponseEntity<CollaboratorEntity> update(@RequestBody CollaboratorEntity collaborator, @RequestHeader("Authorization") String token) throws ParseException {
-        if (!jwtTokenProviderService.getUserIdFromToken(token).equals(collaborator.getId())) {
-            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
-        } else {
-            CollaboratorEntity newCollaborator = collaboratorService.update(collaborator);
-            return new ResponseEntity<>(newCollaborator, HttpStatus.OK);
-        }
-    }
-
-    @CrossOrigin
-    @GetMapping(ADMIN)
-    public ResponseEntity<List<CollaboratorEntity>> getAllAdmin() {
-        List<CollaboratorEntity> collaborators = collaboratorService.findCollaboratorEntitiesWhereIsPopulated();
-        return new ResponseEntity<>(collaborators, HttpStatus.OK);
-    }
-
-    @CrossOrigin
-    @GetMapping(ADMIN + "shift/{shiftId}")
-    public ResponseEntity<List<CollaboratorEntity>> getByShiftId(@PathVariable int shiftId) {
-        List<CollaboratorEntity> collaborators = collaboratorService.getByShiftId(shiftId);
-        return new ResponseEntity<>(collaborators, HttpStatus.OK);
-    }
-
-
-    @CrossOrigin
-    @DeleteMapping(ADMIN)
-    public ResponseEntity<CollaboratorEntity> deleteById(@RequestBody CollaboratorEntity collaboratorEntity) {
-        collaboratorService.deleteById(collaboratorEntity.getId());
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
-
+  @CrossOrigin
+  @DeleteMapping(ADMIN)
+  public ResponseEntity<CollaboratorEntity> deleteById(@RequestBody CollaboratorEntity collaboratorEntity) {
+    collaboratorService.deleteById(collaboratorEntity.getId());
+    return new ResponseEntity<>(HttpStatus.OK);
+  }
 
 }
--- festivalFlow-fe/src/app/pages/admin-area/user-detail/user-detail.component.ts
+++ festivalFlow-fe/src/app/pages/admin-area/user-detail/user-detail.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {CollaboratorService} from "../../../services/http/collaborator.service";
 import {ActivatedRoute, Router} from "@angular/router";
 import {Collaborator} from "../../../interfaces/CollaboratorEntity";
+import {FormBuilder, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-user-detail',
@@ -12,10 +13,22 @@ export class UserDetailComponent implements OnInit {
   protected dataError: boolean = false
   protected collaborator: Collaborator = {} as Collaborator;
 
+  protected collaboratorForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    phone: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(10)]],
+    age: ['', Validators.required],
+    size: ['', Validators.required],
+    yearsExperience: ['', Validators.required],
+    town: ['', Validators.required],
+  });
+
   constructor(
     private collaboratorService: CollaboratorService,
     private route: ActivatedRoute,
     private router: Router,
+    private fb: FormBuilder,
   ) {
   }
 
@@ -24,6 +37,7 @@ export class UserDetailComponent implements OnInit {
       if (params['id']) {
         this.collaboratorService.getCollaboratorById(params['id']).subscribe((collaborator: Collaborator) => {
           this.collaborator = collaborator;
+          this.initForm();
         });
       }
     });
@@ -31,14 +45,51 @@ export class UserDetailComponent implements OnInit {
   }
 
   submitData() {
-    this.collaboratorService.updateCollaborator(this.collaborator).pipe().subscribe(() => {
-      this.router.navigate(['admin/user']);
+    this.collaboratorService.updateCollaboratorFromAdmin(this.getCollaboratorFromFormData()).pipe().subscribe(() => {
+      window.location.reload();
     });
   }
 
   deleteCollaborator() {
+    console.log(this.collaboratorForm);
     this.collaboratorService.deleteCollaborator(this.collaborator).pipe().subscribe(() => {
       this.router.navigate(['admin/user']);
     });
+
+  }
+
+  getCollaboratorFromFormData(): Collaborator {
+    let email = this.collaboratorForm.get('email')?.value;
+    let phone = this.collaboratorForm.get('phone')?.value;
+    let yearsExperience = this.collaboratorForm.get('yearsExperience')?.value;
+
+    if (email && phone && yearsExperience) {
+      return {
+        id: this.collaborator.id,
+        email: email,
+        firstName: this.collaboratorForm.get('firstName')?.value,
+        lastName: this.collaboratorForm.get('lastName')?.value,
+        phone: phone,
+        age: this.collaboratorForm.get('age')?.value,
+        size: this.collaboratorForm.get('size')?.value,
+        yearsExperience: Number(yearsExperience),
+        town: this.collaboratorForm.get('town')?.value,
+      }
+    }
+
+    return {} as Collaborator;
+  }
+
+  private initForm() {
+    if (this.collaborator.firstName && this.collaborator.lastName && this.collaborator.email && this.collaborator.phone && this.collaborator.age && this.collaborator.size && this.collaborator.yearsExperience && this.collaborator.town) {
+      this.collaboratorForm.get('email')?.setValue(this.collaborator.email);
+      this.collaboratorForm.get('firstName')?.setValue(this.collaborator.firstName);
+      this.collaboratorForm.get('lastName')?.setValue(this.collaborator.lastName);
+      this.collaboratorForm.get('phone')?.setValue(this.collaborator.phone);
+      this.collaboratorForm.get('age')?.setValue(this.collaborator.age);
+      this.collaboratorForm.get('size')?.setValue(this.collaborator.size);
+      this.collaboratorForm.get('yearsExperience')?.setValue(String(this.collaborator.yearsExperience));
+      this.collaboratorForm.get('town')?.setValue(this.collaborator.town);
+    }
   }
 }
