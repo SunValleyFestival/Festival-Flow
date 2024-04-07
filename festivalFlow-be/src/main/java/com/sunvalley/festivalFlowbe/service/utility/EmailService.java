958b7e46380cb3e9ac39aa6b9e0fe7397cb8bc41
grata <aramis.grata@bluewin.ch>
2024-04-07 21:11:42 +0200

    feat(102): Edited mail message

--- festivalFlow-be/src/main/java/com/sunvalley/festivalFlowbe/service/utility/EmailService.java
+++ festivalFlow-be/src/main/java/com/sunvalley/festivalFlowbe/service/utility/EmailService.java
@@ -24,156 +24,183 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class EmailService {
 
-    private final JavaMailSender mailSender;
-    private final CollaboratorService collaboratorService;
-    private final ShiftService shiftService;
-    private final DayService dayService;
-    private final LocationService locationService;
+  private final JavaMailSender mailSender;
+  private final CollaboratorService collaboratorService;
+  private final ShiftService shiftService;
+  private final DayService dayService;
+  private final LocationService locationService;
 
-    @Value("${spring.mail.properties.mail.smtp.from}")
-    private String senderAddress;
-    @Value("${spring.mail.properties.enabled}")
-    private boolean enabled;
-    @Value("${spring.mail.properties.test-receiver}")
-    private String testReceiver;
+  @Value("${spring.mail.properties.mail.smtp.from}")
+  private String senderAddress;
+  @Value("${spring.mail.properties.enabled}")
+  private boolean enabled;
+  @Value("${spring.mail.properties.test-receiver}")
+  private String testReceiver;
 
-    public boolean sendEmail(EmailRequest emailRequest) {
-        log.info("Sending email to <{}> with subject<{}>...", emailRequest.getTo(), emailRequest.getSubject());
+  public boolean sendEmail(EmailRequest emailRequest) {
+    log.info("Sending email to <{}> with subject<{}>...", emailRequest.getTo(), emailRequest.getSubject());
 
-        if (!enabled) {
-            log.info("Send email disabled, check configuration");
-            return false;
-        }
-
-        return sendMimeMessage(emailRequest);
+    if (!enabled) {
+      log.info("Send email disabled, check configuration");
+      return false;
     }
 
-    public boolean sendExport(String email, Attachment attachment) {
+    return sendMimeMessage(emailRequest);
+  }
 
-        if (!enabled) {
-            log.info("Send email disabled, check configuration");
-            return false;
-        }
+  public boolean sendExport(String email, Attachment attachment) {
 
-        EmailRequest emailRequest = new EmailRequest();
-        emailRequest.setTo(email);
-        emailRequest.setSubject("Export");
-        emailRequest.setMessage("Export");
+    if (!enabled) {
+      log.info("Send email disabled, check configuration");
+      return false;
+    }
+
+    EmailRequest emailRequest = new EmailRequest();
+    emailRequest.setTo(email);
+    emailRequest.setSubject("Export");
+    emailRequest.setMessage("Export");
 
 //        addTestReceiverIfConfigured(emailRequest);
-        return sendMimeMessageExport(emailRequest, attachment);
+    return sendMimeMessageExport(emailRequest, attachment);
+  }
+
+  public boolean sendCodeViaEmail(String code, int userId) {
+    log.info("Sending code to user <{}>...", userId);
+    var emailRequest = new EmailRequest();
+    String email = collaboratorService.getEmailById(userId);
+    if (email == null || code.isEmpty()) {
+      return false;
     }
+    emailRequest.setTo(email);
+    emailRequest.setSubject("Codice di verifica");
 
-    public boolean sendCodeViaEmail(String code, int userId) {
-        log.info("Sending code to user <{}>...", userId);
-        var emailRequest = new EmailRequest();
-        String email = collaboratorService.getEmailById(userId);
-        if (email == null || code.isEmpty()) {
-            return false;
-        }
-        emailRequest.setTo(email);
-        emailRequest.setSubject("Codice di verifica");
+    //add message with link
+    emailRequest.setMessage("Il tuo codice di verifica é: " + code + "<br><br> Il team di SVF");
+    return sendEmail(emailRequest);
 
-        //add message with link
-        emailRequest.setMessage("Il tuo codice di verifica é: " + code + "<br><br> Il team di SVF");
-        return sendEmail(emailRequest);
+  }
 
+  public void sendNotificationViaEmail(int userId, Status status, int shiftId) {
+    EmailRequest emailRequest = new EmailRequest();
+    emailRequest.setTo(collaboratorService.getEmailById(userId));
+    ShiftEntity shiftEntity;
+    shiftEntity = shiftService.getById(shiftId);
+    var message = "";
+
+    switch (status) {
+      case ACCEPTED:
+        emailRequest.setSubject("Turno accettato!");
+        message = "Il tuo turno: " + shiftEntity.getName() + " alla postazione: " + shiftEntity.getLocation().getName() + " al: " + shiftEntity.getLocation().getDay().getName()
+            + " è stato accettato <br> Il tuo turno inizia alle: " + shiftEntity.getStartTime() + " e finisce alle: " + shiftEntity.getEndTime() + " <br><br>"
+            + collaboratorService.getDataForEmail(userId);
+
+        message +=
+            "<br><br>Ognuno di noi qui contribuisce volontariamente per il Festival e per l'Associazione Asilo Beach: è semplicemente parte della nostra cultura e della nostra identità. Per "
+                + "far stare in piedi il tutto dobbiamo avere RISPETTO ed evitare comportamenti come:<br>";
+        message +=
+            "- Presentarsi in ritardo<br>"
+                + "- Andare in backstage<br>"
+                + "- Presentarsi ubriachi al turno<br>"
+                + "- Ubriacarsi durante il turno<br>"
+                + "- Dare da bere gratis<br>"
+                + "- Entrare nei bar quando non si è di turno";
+
+        message +=
+            " <br><br> Non dimenticate di presentarvi 20min in anticipo al vostro turno! Per domande scrivere a "
+                + "Jail(<a href=\"tel:0796588917\">079 658 89 17</a>) o "
+                + "Bryan (<a href=\"tel:0796510156\">079 651 01 56</a>).";
+
+        break;
+      case REJECTED:
+        emailRequest.setSubject("Turno rifiutato!");
+        message = "Il tuo turno: " + shiftEntity.getName() + " alla postazione: " + shiftEntity.getLocation().getName() + " al: " + shiftEntity.getLocation().getDay().getName()
+            + "è stato rifiutato <br> Il tuo turno sarebbe iniziato alle: " + shiftEntity.getStartTime() + " e finito alle: " + shiftService.getById(shiftId).getEndTime() + " <br><br>"
+            + collaboratorService.getDataForEmail(userId);
+        break;
+      case PENDING:
+        emailRequest.setSubject("Turno in attesa!");
+        message = "Il tuo turno: " + shiftEntity.getName() + " alla postazione: " + shiftEntity.getLocation().getName() + " al: " + shiftEntity.getLocation().getDay().getName()
+            + " è in attesa di approvazione \n Il tuo turno inizia alle: " + shiftEntity.getStartTime() + " e finisce alle: " + shiftService.getById(shiftId).getEndTime() + " <br><br>"
+            + collaboratorService.getDataForEmail(userId);
+        break;
+      default:
+        log.error("Invalid status");
+        return;
     }
+    message += " <br><br> Grazie per la tua collaborazione! <br> Il team SVF!";
 
-    public void sendNotificationViaEmail(int userId, Status status, int shiftId) {
-        EmailRequest emailRequest = new EmailRequest();
-        emailRequest.setTo(collaboratorService.getEmailById(userId));
-        ShiftEntity shiftEntity;
-        switch (status) {
-            case ACCEPTED:
-                shiftEntity = shiftService.getById(shiftId);
-                emailRequest.setSubject("Turno accettato!");
-                emailRequest.setMessage("Il tuo turno: " + shiftEntity.getName() + " alla postazione: " + shiftEntity.getLocation().getName() + " al: " + shiftEntity.getLocation().getDay().getName() + " è stato accettato <br> Il tuo turno inizia alle: " + shiftEntity.getStartTime() + " e finisce alle: " + shiftEntity.getEndTime() + " <br><br>" + collaboratorService.getDataForEmail(userId) + " <br><br> Grazie per la tua collaborazione! <br> Il team di SVF!");
-                break;
-            case REJECTED:
-                shiftEntity = shiftService.getById(shiftId);
-                emailRequest.setSubject("Turno rifiutato!");
-                emailRequest.setMessage("Il tuo turno: " + shiftEntity.getName() + " alla postazione: " + shiftEntity.getLocation().getName() + " al: " + shiftEntity.getLocation().getDay().getName() + "è stato rifiutato <br> Il tuo turno sarebbe iniziato alle: " + shiftEntity.getStartTime() + " e finito alle: " + shiftService.getById(shiftId).getEndTime() + " <br><br>" + collaboratorService.getDataForEmail(userId) + "<br><br> Grazie per la tua collaborazione <br> Il team di SVF");
-                break;
-            case PENDING:
-                shiftEntity = shiftService.getById(shiftId);
-                emailRequest.setSubject("Turno in attesa!");
-                emailRequest.setMessage("Il tuo turno: " + shiftEntity.getName() + " alla postazione: " + shiftEntity.getLocation().getName() + " al: " +shiftEntity.getLocation().getDay().getName() + " è in attesa di approvazione \n Il tuo turno inizia alle: " + shiftEntity.getStartTime() + " e finisce alle: " + shiftService.getById(shiftId).getEndTime()  + " <br><br>" + collaboratorService.getDataForEmail(userId) +" <br><br>  Grazie per la tua collaborazione <br> Il team di SVF");
-                break;
-            default:
-                log.error("Invalid status");
-                return;
-        }
-        sendEmail(emailRequest);
+    emailRequest.setMessage(message);
+
+    sendEmail(emailRequest);
+  }
+
+  public void sendCollabortorInfo(int userId) {
+    EmailRequest emailRequest = new EmailRequest();
+    emailRequest.setTo(collaboratorService.getEmailById(userId));
+    emailRequest.setSubject("Info personali");
+    emailRequest.setMessage(collaboratorService.getDataForEmail(userId) + " <br> Il team di SVF!");
+    sendEmail(emailRequest);
+  }
+
+  private boolean sendMimeMessage(EmailRequest emailRequest) {
+    var mimeMessage = mailSender.createMimeMessage();
+    try {
+      var helper = getMimeMessageHelper(emailRequest, mimeMessage);
+      //addAttachments(helper, emailRequest.getAttachments());
+
+      mailSender.send(mimeMessage);
+      log.info("Email sent");
+      return true;
+
+    } catch (MessagingException e) {
+      log.error("Error sending email", e);
+      return false;
     }
+  }
 
-    public void sendCollabortorInfo(int userId) {
-        EmailRequest emailRequest = new EmailRequest();
-        emailRequest.setTo(collaboratorService.getEmailById(userId));
-        emailRequest.setSubject("Info personali");
-        emailRequest.setMessage(collaboratorService.getDataForEmail(userId)+" <br> Il team di SVF!");
-        sendEmail(emailRequest);
+  private boolean sendMimeMessageExport(EmailRequest emailRequest, Attachment attachment) {
+    var mimeMessage = mailSender.createMimeMessage();
+    try {
+      var helper = getMimeMessageHelper(emailRequest, mimeMessage);
+
+      addAttachments(helper, attachment);
+
+      mailSender.send(mimeMessage);
+      log.info("Email sent");
+      return true;
+
+    } catch (MessagingException e) {
+      log.error("Error sending email", e);
+      return false;
     }
+  }
 
-    private boolean sendMimeMessage(EmailRequest emailRequest) {
-        var mimeMessage = mailSender.createMimeMessage();
-        try {
-            var helper = getMimeMessageHelper(emailRequest, mimeMessage);
-            //addAttachments(helper, emailRequest.getAttachments());
+  private MimeMessageHelper getMimeMessageHelper(EmailRequest emailRequest, MimeMessage mimeMessage) throws MessagingException {
+    var helper = new MimeMessageHelper(mimeMessage, true);
+    helper.setTo(emailRequest.getTo());
+    helper.setSubject(emailRequest.getSubject());
+    helper.setText(emailRequest.getMessage(), true);
+    helper.setFrom(senderAddress);
 
-            mailSender.send(mimeMessage);
-            log.info("Email sent");
-            return true;
+    return helper;
+  }
 
-        } catch (MessagingException e) {
-            log.error("Error sending email", e);
-            return false;
-        }
+  private void addTestReceiverIfConfigured(EmailRequest emailRequest) {
+    if (StringUtils.isNotEmpty(testReceiver)) {
+      log.info("Test receiver configured, email will be forwarded to this address: <{}>", testReceiver);
+      emailRequest.setTo(testReceiver);
     }
+  }
 
-    private boolean sendMimeMessageExport(EmailRequest emailRequest, Attachment attachment) {
-        var mimeMessage = mailSender.createMimeMessage();
-        try {
-            var helper = getMimeMessageHelper(emailRequest, mimeMessage);
+  private void addAttachments(MimeMessageHelper helper, Attachment attachment) {
+    addAttachment(helper, attachment);
+  }
 
-            addAttachments(helper, attachment);
-
-            mailSender.send(mimeMessage);
-            log.info("Email sent");
-            return true;
-
-        } catch (MessagingException e) {
-            log.error("Error sending email", e);
-            return false;
-        }
+  private void addAttachment(MimeMessageHelper helper, Attachment attachment) {
+    try {
+      helper.addAttachment(attachment.getFilename(), new ByteArrayResource(attachment.getContent()));
+    } catch (MessagingException e) {
+      log.error("Error adding attachment", e);
     }
-
-    private MimeMessageHelper getMimeMessageHelper(EmailRequest emailRequest, MimeMessage mimeMessage) throws MessagingException {
-        var helper = new MimeMessageHelper(mimeMessage, true);
-        helper.setTo(emailRequest.getTo());
-        helper.setSubject(emailRequest.getSubject());
-        helper.setText(emailRequest.getMessage(), true);
-        helper.setFrom(senderAddress);
-
-        return helper;
-    }
-
-    private void addTestReceiverIfConfigured(EmailRequest emailRequest) {
-        if (StringUtils.isNotEmpty(testReceiver)) {
-            log.info("Test receiver configured, email will be forwarded to this address: <{}>", testReceiver);
-            emailRequest.setTo(testReceiver);
-        }
-    }
-
-    private void addAttachments(MimeMessageHelper helper, Attachment attachment) {
-        addAttachment(helper, attachment);
-    }
-
-    private void addAttachment(MimeMessageHelper helper, Attachment attachment) {
-        try {
-            helper.addAttachment(attachment.getFilename(), new ByteArrayResource(attachment.getContent()));
-        } catch (MessagingException e) {
-            log.error("Error adding attachment", e);
-        }
-    }
+  }
 }
